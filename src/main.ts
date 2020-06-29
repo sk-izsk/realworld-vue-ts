e90bfa9722ee3ac4c83eaf87657f5185d807c130
Zeeshan <zeeshan@guiker.com>
2020-06-29 20:47:12 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from "vue";
+import { CreateElement } from "vue/types/umd";
+import App from "./App.vue";
+import "./assets/main.css";
+import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: (h: CreateElement) => h(App),
+}).$mount("#app");
