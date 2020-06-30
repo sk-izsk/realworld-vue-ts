3f8f4ecfadbcb0177e1a93fc7b9632232f4ca629
Zeeshan <zeeshan@guiker.com>
2020-06-30 14:31:38 +0530

    add precommit

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
-
 import AppFooter from "@/components/AppFooter.vue";
 import AppNavbar from "@/components/AppNavbar.vue";
 @Component({
---
+++ src/store/modules/user.ts
@@ -0,0 +1,4 @@
+import { Module, VuexModule } from "vuex-module-decorators";
+
+@Module
+export default class UserModule extends VuexModule {}
